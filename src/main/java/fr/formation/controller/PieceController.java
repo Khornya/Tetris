d285b0289cbea075c6aeee753b253cbaf2468f1e
Khornya <33462756+Khornya@users.noreply.github.com>
2020-09-22 15:08:45 +0200

    PieceController

--- src/main/java/fr/formation/controller/PieceController.java
+++ src/main/java/fr/formation/controller/PieceController.java
@@ -1,10 +1,55 @@
 package fr.formation.controller;
 
+import fr.formation.models.Piece;
+import fr.formation.service.PieceService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/piece")
 public class PieceController {
+    @Autowired
+    private PieceService pieceService;
 
+    @GetMapping("/list")
+    public String findAll(Model model) {
+        model.addAttribute("pieces", this.pieceService.findAll());
+
+        return "piece/list";
+    }
+
+    @GetMapping("/add")
+    public String add() {
+        return "piece/form";
+    }
+
+    @PostMapping("/add")
+    public String add(Piece piece) {
+        this.pieceService.add(piece);
+
+        return "redirect:./list";
+    }
+
+    @GetMapping("/edit")
+    public String edit(@RequestParam int id, Model model) {
+        model.addAttribute("piece", this.pieceService.findById(id));
+
+        return "piece/form";
+    }
+
+    @PostMapping("/edit")
+    public String edit(@RequestParam int id, Piece piece) {
+        this.pieceService.edit(id, piece);
+
+        return "redirect:./list";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteById(@PathVariable int id) {
+        this.pieceService.deleteById(id);
+
+        return "redirect:../list";
+    }
 }
